bd8c71697ab8109379858686068587e656bba638
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2025-06-25 18:32:09 +0600

    another topic covered.

---
+++ Practice/operator.py
@@ -0,0 +1,33 @@
+a = 10
+b = 4
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b) #Exponents or power function
+print(a//b) #Return floor values. point value won't come
+
+#Assignment operators:
+a+=2
+print(a)
+b-=3
+print(b)
+a/=2
+print(a)
+b*=2
+print(b)
+
+#Comparison Operator
+# ==, !=, >, <, <=, >=, not
+
+#Logical Operator and, or,
+print(a<b and a>10)
+
+#Membership operators
+st ="I am a student of SUST"
+print('Am' in st)
+
+#Identity operators
+print(a is b)
+print(a is not b)
