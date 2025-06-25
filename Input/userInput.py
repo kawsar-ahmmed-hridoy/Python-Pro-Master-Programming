70ad0f84740c1b42aedf40d9074f79a17f9c8a9e
kawsar_ahmmed_hridoy <kawsarhridoy0146@gmail.com>
2025-06-26 01:33:09 +0600

    new

---
+++ Input/userInput.py
@@ -0,0 +1,14 @@
+a = input() # a = input("Take a input: ")
+print(a)
+
+arr1 = list(map(int,input().split()))
+print(arr1)
+
+n = int(input("Enter the size of the array: "))
+arr2 = [0]*n
+for i in range(n):
+    arr2[i] = int(input()) #input: 2
+                        #          4
+                        #          8
+
+print(arr2)
